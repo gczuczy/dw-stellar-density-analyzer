ca0e3ce8e3205412984e90091e43ee0062591712
Gergely Czuczy <gergely.czuczy@harmless.hu>
2026-01-28 20:44:33 +0100

    Database sketch

---
+++ sql/struct.sql
@@ -0,0 +1,52 @@
+DROP SCHEMA IF EXISTS density CASCADE;
+
+CREATE SCHEMA density;
+GRANT USAGE ON SCHEMA density TO edadmin, edservice, edviewer;
+
+CREATE TABLE density.projects (
+       id int GENERATED ALWAYS AS IDENTITY,
+       name varchar(64) NOT NULL,
+       PRIMARY KEY (id)
+);
+GRANT SELECT, INSERT, UPDATE, DELETE ON density.projects TO edservice;
+GRANT SELECT ON density.projects TO edviewer;
+INSERT INTO density.projects (name) VALUES
+('A15X CW Density Scans'),
+('DW3 Stellar Density'),
+('DW3 Logarithmic Density Scans')
+;
+
+CREATE TABLE density.cmdrs (
+       id    int		  GENERATED ALWAYS AS IDENTITY,
+       name  varchar(64)	  NOT NULL UNIQUE,
+       PRIMARY KEY (id)
+);
+GRANT SELECT, INSERT, UPDATE, DELETE ON density.cmdrs TO edservice;
+GRANT SELECT ON density.cmdrs TO edviewer;
+
+CREATE TABLE density.measurements (
+       id    int		  GENERATED ALWAYS AS IDENTITY,
+       projectid int		  NOT NULL,
+       cmdrid	 int		  NOT NULL,
+       FOREIGN KEY (projectid) REFERENCES density.projects (id),
+       FOREIGN KEY (cmdrid) REFERENCES density.cmdrs(id),
+       PRIMARY KEY (id)
+);
+GRANT SELECT, INSERT, UPDATE, DELETE ON density.measurements TO edservice;
+GRANT SELECT ON density.measurements TO edviewer;
+
+CREATE TABLE density.datapoints (
+       id    int		  GENERATED ALWAYS AS IDENTITY,
+       measurementid int	  NOT NULL,
+       x	     real	  NOT NULL,
+       y	     real	  NOT NULL,
+       z	     real	  NOT NULL,
+       syscount	     int	  NOT NULL,
+       maxdistance   real	  NOT NULL,
+       FOREIGN KEY (measurementid) REFERENCES density.measurements(id),
+       PRIMARY KEY (id),
+       CHECK (syscount >= 0 AND syscount <= 50),
+       CHECK (maxdistance > 0 AND maxdistance <= 20)
+);
+GRANT SELECT, INSERT, UPDATE, DELETE ON density.datapoints TO edservice;
+GRANT SELECT ON density.datapoints TO edviewer;
